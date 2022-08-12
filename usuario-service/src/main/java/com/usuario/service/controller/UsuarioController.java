c761580f9348f464dff49c89271c7ed368de7863
William Novoa <wjnovoam@ufpso.edu.co>
2022-08-12 13:42:27 -0500

    circuit breaker

--- usuario-service/src/main/java/com/usuario/service/controller/UsuarioController.java
+++ usuario-service/src/main/java/com/usuario/service/controller/UsuarioController.java
@@ -4,7 +4,9 @@ import com.usuario.service.entity.Usuario;
 import com.usuario.service.models.Carro;
 import com.usuario.service.models.Moto;
 import com.usuario.service.service.UsuarioService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,6 +52,7 @@ public class UsuarioController {
         return ResponseEntity.ok(nuevoUsuario);
     }
 
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackGetCarros")
     @GetMapping("/carros/{usuarioId}")
     public ResponseEntity<List<Carro>> listarCarros(@PathVariable("usuarioId") Long id){
         Usuario usuario = usuarioService.getUsuarioById(id);
@@ -63,6 +66,7 @@ public class UsuarioController {
         return ResponseEntity.ok(carros);
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackGetMotos")
     @GetMapping("/motos/{usuarioId}")
     public ResponseEntity<List<Moto>> listarMotos(@PathVariable("usuarioId") Long id){
         Usuario usuario = usuarioService.getUsuarioById(id);
@@ -76,22 +80,47 @@ public class UsuarioController {
         return ResponseEntity.ok(motos);
     }
 
+    @CircuitBreaker(name = "carrosCB", fallbackMethod = "fallBackSaveCarro")
     @PostMapping("carro/{usuarioId}")
     public ResponseEntity<Carro> guardarCarro(@PathVariable("usuarioId") Long usuarioId, @RequestBody Carro carro){
         Carro nuevoCarro = usuarioService.saveCarro(usuarioId, carro);
         return ResponseEntity.ok(nuevoCarro);
     }
 
+    @CircuitBreaker(name = "motosCB", fallbackMethod = "fallBackSaveMoto")
     @PostMapping("moto/{usuarioId}")
     public ResponseEntity<Moto> guardarMoto(@PathVariable("usuarioId") Long usuarioId, @RequestBody Moto moto){
         Moto nuevaMoto = usuarioService.saveMoto(usuarioId, moto);
         return ResponseEntity.ok(nuevaMoto);
     }
 
+    @CircuitBreaker(name = "todosCB", fallbackMethod = "fallBackGetTodos")
     @GetMapping("/todos/{usuarioId}")
     public ResponseEntity<Map<String, Object>> listarTodosLosVehiculos(@PathVariable("usuarioId") Long usuarioId){
         Map<String, Object> resultado = usuarioService.getUsuarioAndVehiculos(usuarioId);
         return ResponseEntity.ok(resultado);
     }
 
+    private ResponseEntity<List<Carro>> fallBackGetCarros(@PathVariable("usuarioId") Long id, RuntimeException exception){
+        return new ResponseEntity("El usuario: "+id+" tiene los carros en el taller", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<Carro>> fallBackSaveCarro(@PathVariable("usuarioId") Long id, @RequestBody Carro carro,
+                                                          RuntimeException exception){
+        return new ResponseEntity("El usuario: "+id+" no tiene dinero para los carros", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<Moto>> fallBackGetMotos(@PathVariable("usuarioId") Long id, RuntimeException exception){
+        return new ResponseEntity("El usuario: "+id+" tiene las motos en el taller", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<Moto>> fallBackSaveMoto(@PathVariable("usuarioId") Long id, @RequestBody Moto moto,
+                                                          RuntimeException exception){
+        return new ResponseEntity("El usuario: "+id+" no tiene dinero para las motos", HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<Moto>> fallBackGetTodos(@PathVariable("usuarioId") Long id, @RequestBody Moto moto,
+                                                        RuntimeException exception){
+        return new ResponseEntity("El usuario: "+id+" tiene los vehiculos en el taller", HttpStatus.OK);
+    }
 }
